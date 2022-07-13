71ef73a7fd93fa22a2aa0b8390f82e340967c527
scud <guillaume.scudetti@gmail.com>
2022-07-13 23:31:50 +0200

    h-icon

--- layouts/default.vue
+++ layouts/default.vue
@@ -41,7 +41,7 @@
         <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
       </v-btn>
         <v-spacer />
-      <v-toolbar-title v-text="title" id="temp-title" />
+      <img class="logo-white" src="../logo/logo.png"/><v-toolbar-title v-text="title" id="temp-title" />
       <v-spacer />
     <v-btn  @click="rightDrawer = !rightDrawer" id="temp-user">username</v-btn>
       
@@ -85,7 +85,7 @@
       :absolute="!fixed"
       app
     >
-      <span>&copy; Groupomania le reseau dev by scud</span>
+      <span>&copy;   Groupomania le reseau dev by scud </span>
     </v-footer>
   </v-app>
 </template>
@@ -100,7 +100,7 @@ export default {
       fixed: false,
       itemsuser:[
         {
-      icon: 'mdi-chart-bubble',
+      icon: 'mdi-lan-disconnect',
       title: 'se deconnecter',
       to: '/'
       } , 
@@ -110,12 +110,12 @@ export default {
      to: '/'
       } , 
       {
-      icon: 'mdi-chart-bubble',
+      icon: 'mdi-thumb-up-outline ',
      title: 'mes likes',
      to: '/'
       } ,  
       {
-      icon: 'mdi-chart-bubble',
+      icon: 'mdi-account',
      title: 'mon profil',
      to: '/'
       } , 
@@ -128,7 +128,7 @@ export default {
         },
         {
           icon: 'mdi-chart-bubble',
-          title: 'le Grou-Post',
+          title: 'le Grou-Post-mania',
           to: '/post'
         }
       ],
@@ -161,19 +161,22 @@ width: auto;
 border: solid $secondary;
 }
 #temp-title{
-
-  font-family: Lato, sans-serif;
-  font-size: 35px;
-  letter-spacing: 2px;
+font-family: Lato, sans-serif;
+font-size: 35px;
+letter-spacing: 2px;
 animation:textAnimated 30s linear infinite reverse;
 @keyframes textAnimated {
-	0% {background:linear-gradient(70deg, darken($primary,20%),$tertiary, $secondary); background-position:-12em 0; background-clip:text; -webkit-background-clip:text; color:transparent;}
-	50% {background:linear-gradient(85deg, darken($primary,20%),$tertiary, $secondary); background-position:0 0; background-clip:text; -webkit-background-clip:text; color:transparent;}
-	100% {background:linear-gradient(70deg, darken($primary,20%),$tertiary, $secondary); background-position:-12em; background-clip:text; -webkit-background-clip:text; color:transparent;}
+0% {background:linear-gradient(70deg, darken($primary,20%),$tertiary, $secondary); background-position:-12em 0; background-clip:text; -webkit-background-clip:text; color:transparent;}
+50% {background:linear-gradient(85deg, darken($primary,20%),$tertiary, $secondary); background-position:0 0; background-clip:text; -webkit-background-clip:text; color:transparent;}
+100% {background:linear-gradient(70deg, darken($primary,20%),$tertiary, $secondary); background-position:-12em; background-clip:text; -webkit-background-clip:text; color:transparent;}
+};
 };
 
-};
-
+.logo-white{
+margin-right: 1%;
+height: 30px;
+width: 30px;
+}
 
 
 </style>
