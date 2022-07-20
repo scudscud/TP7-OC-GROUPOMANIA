e43f19e516631748f2ef2c80528357c3957f4af0
scud <guillaume.scudetti@gmail.com>
2022-07-20 06:23:16 +0200

    test save

---
+++ backend/models/user.model.js
@@ -0,0 +1,29 @@
+const mongoose=require('mongoose');
+const {email}= require('validator');
+
+const userSchema =new mongoose.Schema(
+{
+
+    pseudo:{
+    type: String,
+    required:true,
+    minLenght
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+)
